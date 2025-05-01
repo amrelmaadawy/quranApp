5fb5504fcb08f6c9b3d1594498320661e8bcc742
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-05-01 17:27:19 +0300

    add calender view

--- lib/views/allduas/UI/allduas.dart
+++ lib/views/allduas/UI/allduas.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quranapp/core/components/custom_appbar_worships.dart';
 
-class AllDuas  extends StatelessWidget {
-  const AllDuas ({super.key});
+class AllDuas extends StatelessWidget {
+  const AllDuas({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,10 @@ class AllDuas  extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.end,
-              children: [CustomAppBar(title: 'جميع الادعيه'),],
+              children: [
+                CustomAppBar(title: 'جميع الادعيه'),
+
+              ],
             ),
           ),
         ),
