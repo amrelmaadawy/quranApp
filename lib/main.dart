a95fc7104a132d73a34900f27cf0da3f9d89068c
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-05-01 14:47:36 +0300

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MainHomeView());
+    return MaterialApp(
+      theme: ThemeData(fontFamily: "Amiri"),
+      
+      debugShowCheckedModeBanner: false, home: MainHomeView());
   }
 }
--- lib/views/setting/UI/setting.dart
+++ lib/views/setting/UI/setting.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:quranapp/core/app_color.dart';
 
 class Setting extends StatelessWidget {
   const Setting({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('setting');
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'الاعدادات',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: AppColor.kprimryColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Icon(Icons.settings, color: AppColor.kprimryColor, size: 32),
+                Text('Test'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
