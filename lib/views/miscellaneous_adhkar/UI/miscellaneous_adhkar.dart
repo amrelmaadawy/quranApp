b85e08f92a3492f16082e4695aa5f26dba4cdfe0
amrelmaadawy <amrelmaadawy905@gmail.com>
2025-05-03 21:13:15 +0300

    finish miscellaneous adhkar view

--- lib/views/miscellaneous_adhkar/UI/miscellaneous_adhkar.dart
+++ lib/views/miscellaneous_adhkar/UI/miscellaneous_adhkar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quranapp/core/components/adhkar_card.dart';
 import 'package:quranapp/core/components/custom_appbar_worships.dart';
+import 'package:quranapp/core/get_data.dart';
+import 'package:quranapp/core/model/adhkar/adhkar.dart';
 
 class MiscellaneousAdhkar extends StatelessWidget {
   const MiscellaneousAdhkar({super.key});
@@ -12,20 +14,105 @@ class MiscellaneousAdhkar extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                CustomAppBar(title: 'أذكار متنوعه'),
-                ListView.builder(
-                  itemBuilder:
-                      (context, index) =>
-                          AdhkarCard(alzekr: '', descrption: ''),
-                  itemCount: 10,
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                ),
-              ],
+            child: FutureBuilder(
+              future: GetAdhkar.getAdhkar(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return SnackBar(content: Text('Error'));
+                } else if (snapshot.hasData) {
+                  var items = snapshot.data as List<Adhkar>;
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      CustomAppBar(title: 'أذكار متنوعه'),
+                      ListView.builder(
+                        itemBuilder: (context, index) {
+                          return Card(
+                            margin: const EdgeInsets.all(5),
+                            color: Colors.white,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) {
+                                        return Scaffold(
+                                          body: SingleChildScrollView(
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(
+                                                15.0,
+                                              ),
+                                              child: Column(
+                                                children: [
+                                                  CustomAppBar(
+                                                    title:
+                                                        items[index].category ??
+                                                        '',
+                                                  ),
+                                                  ListView.builder(
+                                                    itemBuilder: (
+                                                      context,
+                                                      listindex,
+                                                    ) {
+                                                      return AdhkarCard(
+                                                        alzekr:
+                                                            items[index]
+                                                                .array![listindex]
+                                                                .text ??
+                                                            '',
+                                                        descrption: '',
+                                                      );
+                                                    },
+                                                    itemCount:
+                                                        items[index]
+                                                            .array!
+                                                            .length,
+                                                    shrinkWrap: true,
+                                                    physics:
+                                                        NeverScrollableScrollPhysics(),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    Icon(Icons.arrow_back),
+
+                                    Spacer(),
+                                    Text(
+                                      items[index].category ?? '',
+                                      style: TextStyle(fontSize: 18),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                        itemCount: items.length,
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                      ),
+                    ],
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
             ),
           ),
         ),
